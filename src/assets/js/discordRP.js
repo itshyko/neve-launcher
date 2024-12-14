f32a31a1663dee0fe85fa89849bdf847b08e6da6
HyKo <happy.plan7983@rt.hyko.dev>
2024-12-13 19:50:14 -0500

    2.0.9

--- src/assets/js/discordRP.js
+++ src/assets/js/discordRP.js
@@ -10,7 +10,7 @@ const activity = {
     state: 'Dans le launcher',
     startTimestamp,
     largeImageKey: 'launcher-neve-2',
-    largeImageText: 'Version 2.0.6',
+    largeImageText: 'Version 2.0.9',
     smallImageKey: 'neve',
     smallImageText: 'Neve Launcher',
     instance: false,
